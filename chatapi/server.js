cce47ec077653664ae7e9c1860af0faf32bd357d
Thomas Brunström <choice@warzone.se>
2021-09-07 14:41:03 +0200

    Added places endpoint to server

--- chatapi/server.js
+++ chatapi/server.js
@@ -33,3 +33,38 @@ app.delete("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Chatapp is listening at ${port}`);
 });
+
+app.get("/places.json", (req, res) => {
+  const p = [
+    { name: "Bysjön", lat: "60.534488156", lng: "15.398712158" },
+    { name: "Torsång motormuseum", lat: "60.4629223", lng: "15.5554419" },
+    {
+      name: "Sahlins Struts",
+      lat: "60.453578003119",
+      lng: "15.492610931397",
+    },
+    {
+      name: "Lindön",
+      lat: "60.500652202873",
+      lng: "15.58170318577",
+    },
+    {
+      name: "Tvärstupet",
+      lat: "60.369665197",
+      lng: "15.430641174",
+    },
+    {
+      name: "Kungsladan",
+      lat: "60.475643285",
+      lng: "15.721135139",
+    },
+  ];
+  const rnd = Math.floor(Math.random() * 100) + 1;
+  if (rnd > 80) {
+    res.status(500).json({ status: "nok", msg: "The server went boo boo" });
+  } else if (rnd > 60) {
+    res.status(403).end();
+  } else {
+    res.json(p);
+  }
+});
